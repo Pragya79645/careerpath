eeb336150af74c49f7024553be301a5c7c1c1960
Pragya79645 <pragya220898@gmail.com>
2025-04-18 16:50:30 +0530

    changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,16 +1,18 @@
-// Import the functions you need from the SDKs you need
+// firebaseConfig.js
+
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAxm7qtwA-HnVlbNa0nbvkJ2GVDku38VIQ",
-  authDomain: "careerpathnavigator-8783c.firebaseapp.com",
-  projectId: "careerpathnavigator-8783c",
-  storageBucket: "careerpathnavigator-8783c.firebasestorage.app",
-  messagingSenderId: "411185912011",
-  appId: "1:411185912011:web:03fb1b13a0aae0fe33634e",
-  measurementId: "G-F14VF8W9RJ"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
@@ -19,7 +21,8 @@ let analytics;
 if (typeof window !== "undefined") {
   analytics = getAnalytics(app);
 }
+
 const firestore = getFirestore(app);
-const auth = getAuth(app); 
-const db = getFirestore(app); // Initialize Firestore
-export { app, analytics, firestore, auth , db};
+const auth = getAuth(app);
+
+export { app, analytics, firestore, auth };
