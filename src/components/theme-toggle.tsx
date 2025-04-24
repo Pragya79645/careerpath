65f1c9676c34f19d29072f1f2c60cd1a10c2eabe
Pragya79645 <pragya220898@gmail.com>
2025-04-25 00:06:24 +0530

    toggle removed

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -17,11 +17,7 @@ export function ThemeToggle() {
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>Light</DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>Dark</DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>System</DropdownMenuItem>
-      </DropdownMenuContent>
+    
     </DropdownMenu>
   )
 }
